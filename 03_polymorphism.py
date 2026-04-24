6a6bbb8a86f9f1f6d70661a2d87db592f2ffa008
Dhanushka De Silva <hadhanushkadesilva@gmail.com>
2026-04-24 18:36:11 +0530

    Add polymorphism example
    Animal parent class with Dog, Cat and Duck showing polymorphism

---
+++ 03_polymorphism.py
@@ -0,0 +1,20 @@
+class Animal:
+    def speak(self):
+        return "Some sound"
+
+class Dog(Animal):
+    def speak(self):
+        return "Woof!"
+
+class Cat(Animal):
+    def speak(self):
+        return "Meow!"
+
+class Duck(Animal):
+    def speak(self):
+        return "Quack!"
+
+animals = [Dog(), Cat(), Duck()]
+
+for animal in animals:
+    print(animal.speak())
